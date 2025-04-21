8a76d1405b9bd9b702dd707e497ca4dc3268871f
characat0 <36453977+characat0@users.noreply.github.com>
2025-04-20 22:53:56 -0500

    Refactor npm package structure to use bin directory

--- npm/bin/index.js
+++ npm/bin/index.js
@@ -5,7 +5,7 @@ const childProcess = require('child_process');
 const package = "databricks-mcp-server";
 
 const BINARY_MAP = {
-    darwin_x64: {name: `${package}-amd64`, suffix: ''},
+    darwin_x64: {name: `${package}-darwin-amd64`, suffix: ''},
     darwin_arm64: {name: `${package}-darwin-arm64`, suffix: ''},
     linux_x64: {name: `${package}-linux-amd64`, suffix: ''},
     linux_arm64: {name: `${package}-linux-arm64`, suffix: ''},
