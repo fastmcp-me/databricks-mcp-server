092baf213ff6a5065583230e7c5b718d4640be7f
characat0 <36453977+characat0@users.noreply.github.com>
2025-04-20 23:44:34 -0500

    Bump version from 0.0.6 to 0.0.7

--- version.go
+++ version.go
@@ -3,7 +3,7 @@ package main
 // Version information
 var (
 	// Version is the current version of the application
-	Version = "0.0.6"
+	Version = "0.0.7"
 
 	// BuildDate is the date when the binary was built
 	BuildDate = "unknown"
